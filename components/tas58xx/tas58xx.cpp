da088d1a1f2144b5b586bf2a64c4245223eef634
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2026-02-25 22:39:08 +1000

    Update tas58xx.cpp

--- components/tas58xx/tas58xx.cpp
+++ components/tas58xx/tas58xx.cpp
@@ -68,9 +68,9 @@ bool Tas58xxComponent::configure_registers_() {
   // enable Tas58xx
   if (!this->set_deep_sleep_off_()) return false;
 
-  if (!this->set_modulation_scheme_(this->tas58xx_modulation_scheme_)) return false;
-
   if (!this->set_dac_mode_(this->tas58xx_dac_mode_)) return false;
+
+  if (!this->set_modulation_scheme_(this->tas58xx_modulation_scheme_)) return false;
 
   if (!this->set_analog_gain_(this->tas58xx_analog_gain_)) return false;
 
@@ -246,16 +246,16 @@ void Tas58xxComponent::dump_config() {
       ESP_LOGCONFIG(TAG,
               "  Registers Configured: %i\n"
               "  Analog Gain: %3.1fdB\n"
-              "  Modulation: %s\n"
               "  DAC Mode: %s\n"
+              "  Modulation: %s\n"
               "  Mixer Mode: %s\n"
               "  Volume Maximum: %idB\n"
               "  Volume Minimum: %idB\n"
               "  Ignore Fault: %s\n"
               "  Refresh EQ: %s\n",
               this->number_registers_configured_, this->tas58xx_analog_gain_,
-              this->tas58xx_modulation_scheme_ ? "1SPW Mode" : "BD Mode",
               this->tas58xx_dac_mode_ ? "PBTL" : "BTL",
+              this->tas58xx_modulation_scheme_ ? "1SPW Mode" : "BD Mode",
               MIXER_MODE_TEXT[this->tas58xx_mixer_mode_],
               this->tas58xx_volume_max_, this->tas58xx_volume_min_,
               this->ignore_clock_faults_when_clearing_faults_ ? "CLOCK FAULTS" : "NONE",
