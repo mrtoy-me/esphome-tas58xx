0c47356e901ee8756d9290cfd8e30e1c74812e47
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2026-03-29 21:57:23 +1000

    add eq qfactor calc
    test calc eq
    add eq qfactor calc

--- components/tas58xx/tas58xx.h
+++ components/tas58xx/tas58xx.h
@@ -161,6 +161,7 @@ class Tas58xxComponent : public audio_dac::AudioDac, public PollingComponent, pu
 
    bool read_fault_registers_();
 
+   void calc_eq_biquad_();
 
    // low level functions
    bool book_and_page_write_(uint8_t book, uint8_t page, uint8_t sub_addr, uint8_t* data, uint8_t number_bytes);
