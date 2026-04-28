29b9fd2db1704b8ac83584548a8d740b7db50c68
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2026-04-28 23:11:38 +1000

    Update tas58xx.cpp

--- components/tas58xx/tas58xx.cpp
+++ components/tas58xx/tas58xx.cpp
@@ -490,7 +490,7 @@ bool Tas58xxComponent::set_eq_gain(Channels channel, uint8_t band_index, int8_t 
     ESP_LOGW(TAG, "%s writing Biquad %s Channel %s:%d Gain: %ddB", ERROR, LR_CHANNEL_TEXT[channel], EQ_BAND, band, gain);
     return false;
   }
-  ESP_LOGD(TAG, "%s Channel %s:%dHz Gain >> %ddB time >> %dus", LR_CHANNEL_TEXT[channel], EQ_BAND, EQ_BAND_FREQUENCY[band_index], gain);
+  ESP_LOGD(TAG, "%s Channel %s:%dHz Gain >> %ddB", LR_CHANNEL_TEXT[channel], EQ_BAND, EQ_BAND_FREQUENCY[band_index], gain);
 #endif
   return true;
 }
